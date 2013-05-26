55566f20da9dd7a49aa393ef31c99b4f947ed346
Jesse Cummins <me@jessecummins.com>
2013-05-26 16:53:28 -0400

    Add button to get file and add file's text to edit box

--- many_file_editor.rb
+++ many_file_editor.rb
@@ -6,12 +6,17 @@
 
 
 Shoes.app :title => "Many File Editor" do
-
+  stack do
+    button "Get File" do
+      filename = ask_open_file
+      edit_box File.read(filename)
+    end
+  end
 end
 
 =begin
 
-- ask to open text file
+- button to open text file, put in edit box
   - select filenames inside to open later
     - copy and paste them into another edit box on the right
   - modify filenames according to rules
