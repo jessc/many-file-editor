bcec0ccf9860a5d6dfe896bb0fd138cbb3625e21
Jesse Cummins <me@jessecummins.com>
2013-05-26 16:41:49 -0400

    Add comment of general flow of app

--- many_file_editor.rb
+++ many_file_editor.rb
@@ -9,3 +9,17 @@ Shoes.app :title => "Many File Editor" do
 
 end
 
+=begin
+
+- ask to open text file
+  - select filenames inside to open later
+    - copy and paste them into another edit box on the right
+  - modify filenames according to rules
+- close main file
+- open each filename
+  - edit box, previous/next/save buttons
+  - make sure Unicode works
+  - next closes file, opens next
+- exit button closes app
+
+=end
