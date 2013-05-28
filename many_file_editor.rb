7188ff8f32812d9413e5abf3d08b6c98b8067aaf
Jesse Cummins <me@jessecummins.com>
2013-05-28 15:22:33 -0400

    Add another edit box on the right side

--- many_file_editor.rb
+++ many_file_editor.rb
@@ -4,17 +4,33 @@
 # Jesse Cummins
 # https://github.com/jessc
 
+=begin
+# Bug List:
+
+- why isn't copy/paste working from one edit box to another?
+
+# TODO:
+=end
+
 
 Shoes.app :title => "Many File Editor" do
-  stack :margin => 10, :height => '95%' do
-    para "Get the file with the file names."
-    button "Get File" do
-      filename = ask_open_file
-      @box.text = File.read(filename)
+  flow :width => '100%', :height => '100%', :margin => 10 do
+    stack :width => '48%', :height => '100%' do
+      para "Get the file with the file names."
+      button "Get File" do
+        filename = ask_open_file
+        @left_box.text = File.read(filename)
+      end
+      @left_box = edit_box :width => '95%', :height => '85%'
     end
-    @box = edit_box :width => '45%', :height => '85%'
+    stack :width => '48%', :height => '100%' do
+      para "Move list of file names here."
+      button "Next Step"
+      @right_box = edit_box :width => '95%', :height => '85%'
+    end
   end
 end
+
 
 =begin
 
