d97d68bb4beec1f6ac5238f1a304ac9218b71d6f
Jesse Cummins <me@jessecummins.com>
2013-06-03 11:53:58 -0400

    Change back to a class system with the windows split apart that can be shown/hidden

--- many_file_editor.rb
+++ many_file_editor.rb
@@ -5,65 +5,69 @@
 # https://github.com/jessc
 # With Shoes inspiration from Dan Garland:
 # https://github.com/dmgarland/TrafficLight
-
+# And inspiration from ashbb:
+# https://github.com/shoes/shoes/issues/236#issuecomment-18843728
 
 =begin
 # Bug List:
 # TODO:
 =end
 
-# Trying a class-less system.
-Shoes.app :title => "Many File Editor" do
-  @main_window =
-    flow :width => '100%', :height => '100%', :margin => 10 do
-      stack :width => '48%', :height => '100%' do
-        para "Paste Near-Filenames Here"
-        @near_filenames = edit_box.text
-        para "Folder Location"
-        @folder_location = edit_line.text
-        # glob the list of files at folder_location
-        # later open the files that match the regex
-       end
-      stack :width => '48%', :height => '100%' do
-        para "Place Regex Here"
-        @regex = edit_line.text
-        button "Apply Regex" do; end
-        @filenames_list = edit_box.text
-        button "Open Files" do; end
-        button "Editing Window" do
-          @main_window.hide()
-          @editing_window.show()
+
+class ManyFileEditor
+  def initialize(foobar)
+    main_window(foobar)
+  end
+
+  def main_window(foobar)
+    s = self
+    foobar.app do
+      @main_window = 
+        # why with ":height => '95%'" on flow and stack doesn't it work?
+        flow :margin => 10, :width => '96%' do 
+          stack :width => '48%' do
+            para "Paste Near-Filenames Here"
+            @near_filenames = edit_box.text
+            para "Folder Location"
+            @folder_location = edit_line.text
+            # glob the list of files at folder_location
+            # later open the files that match the regex
+           end
+          stack :width => '48%' do
+            para "Place Regex Here"
+            @regex = edit_line.text
+            button "Apply Regex" do; end
+            @filenames_list = edit_box.text
+            button "Open Files" do; end
+            button "Editing Window" do
+              @main_window.hide()
+              @editing_window = s.editing_window(foobar)
+              @editing_window.show()
+            end
+          end
+        end
+    end
+  end
+
+  def editing_window(foobar)
+    f = nil
+    foobar.app do
+      f = flow do
+        button "Main Window" do
+          @editing_window.hide()
+          @main_window.show()
         end
       end
     end
+    f
+  end
+end
 
-  @editing_window =
-    flow do
-      # why isn't this button showing @main_window?
-      # it only hides @editing_window
-      button "Main Window" do
-        @editing_window.hide()
-        @main_window.show()
-      end
-    end
-
-  @editing_window.hide()
+Shoes.app :title => "Many File Editor" do
+  @many_file_editor = ManyFileEditor.new(self)
 end
 
 
-
-
-# placeholder stuff:
-    # main_window(flow)
-  # end
-
-  # def main_window
-  # end
-
-  # def editing_window
-  #   para "placeholder"
-  # end
-# end
 
 # placeholder stuff:
 # Shoes.app :title => "Many File Editor" do
